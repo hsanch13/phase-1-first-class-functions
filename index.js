06a132d4c25563094abdb4e22f05e8fcd3f25381
hsanch13 <hayleysanchez13@gmail.com>
2023-11-03 15:46:08 -0600

    made functions

---
+++ index.js
@@ -0,0 +1,15 @@
+
+function receivesAFunction(callback) {
+    callback();
+}
+
+function returnsANamedFunction() {
+    return function tequilla(){
+    }
+}
+
+function returnsAnAnonymousFunction() {
+    return function(){
+        
+    }
+}
